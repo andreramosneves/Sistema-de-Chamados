e9787d9b52ee57f6183a8a653936c39a525400d1
linolica1 <linolica1@linolica-PC.home>
2016-05-01 13:02:11 -0300

    Teste de Cadastro de Empresa - Realiza Cadastro de Empresas novas, com valor automático, - Realiza Cadastro com uma Mesma Empresa

--- test/Testes/TesteCadastrarEmpresa.java
+++ test/Testes/TesteCadastrarEmpresa.java
@@ -5,8 +5,12 @@
  */
 package Testes;
 
+import Persistencia.EmpresaDAO;
 import controle.ControleEmpresas;
 import controle.IControlador;
+import entidade.Empresa;
+import java.util.Iterator;
+import java.util.Random;
 import junit.framework.Assert;
 import junit.framework.TestCase;
 import org.junit.Test;
@@ -16,12 +20,49 @@ import org.junit.Test;
  * @author home
  */
 public class TesteCadastrarEmpresa {
-    
-    
+
     @Test
-    public void testInsercaoMesmaEmpresa()   {
+    public void testInsercaoMesmaEmpresa() {
+        Random rand = new Random();
+        EmpresaDAO mapeadorEmpresa = new EmpresaDAO();
+        Iterator<Empresa> iEmpresas = mapeadorEmpresa.getEmpresas().iterator();
         IControlador controle = new ControleEmpresas();
-        TestCase.assertEquals(controle.inserir(2, ""),null);
+        while (iEmpresas.hasNext()) {
+            Empresa empresa = iEmpresas.next();
+            TestCase.assertEquals(controle.inserir(empresa.getNumeroContrato(), empresa.getNomeEmpresa()), null);
+            break;
+        }
     }
-    
+    @Test
+    public void testNovaEmpresa() {
+        Random rand = new Random();
+        EmpresaDAO mapeadorEmpresa = new EmpresaDAO();
+        Iterator<Empresa> iEmpresas = mapeadorEmpresa.getEmpresas().iterator();
+        IControlador controle = new ControleEmpresas();
+        Long nroContrato;
+        String nome;
+            nroContrato = (long) rand.nextInt();
+            nome = String.valueOf(rand.nextInt()) + 
+                    String.valueOf(rand.nextInt()) + 
+                    String.valueOf(rand.nextInt()) + String.valueOf(rand.nextInt());
+        
+        while (iEmpresas.hasNext()) {
+            Empresa empresa = iEmpresas.next();
+            while(nroContrato == empresa.getNumeroContrato() ){
+                nroContrato = rand.nextLong();
+            }
+            while(nome.equals(empresa.getNomeEmpresa())){
+            nome = String.valueOf(rand.nextInt()) + 
+                    String.valueOf(rand.nextInt()) + 
+                    String.valueOf(rand.nextInt()) + 
+                    String.valueOf(rand.nextInt()) + 
+                    String.valueOf(rand.nextInt()) + 
+                    String.valueOf(rand.nextInt()) + String.valueOf(rand.nextInt());
+            }
+            
+        }
+        TestCase.assertNotNull(controle.inserir(nroContrato,nome));
+
+    }
+
 }
