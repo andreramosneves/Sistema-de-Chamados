d2dd6d5d22982d806996cd3fc72c1324b0fbb36e
home <home@192.168.10.104>
2016-05-01 13:49:19 -0300

    classe teste cliente empresa

--- test/Testes/TesteCadastrarChamado.java
+++ test/Testes/TesteCadastrarChamado.java
@@ -5,10 +5,41 @@
  */
 package Testes;
 
+import Persistencia.ChamadoDAO;
+import controle.ControleChamados;
+import entidade.Chamado;
+import org.junit.Test;
+
 /**
  *
  * @author home
  */
 public class TesteCadastrarChamado {
+    
+    @Test
+    public void TestCriarChamado(){
+        ChamadoDAO chamDAO = new ChamadoDAO();
+        ControleChamados ctrChamados = new ControleChamados();
+        
+       String titulo = " teste de banco";
+       String descricao = " banco de dados nao funciona";
+       String so = "linux";
+       String versao = " debian";
+        
+        Chamado chambanco=ctrChamados.InserirChamadoBancoDeDados(titulo, descricao, 0, tecnico, cliente, so, versaoSO, bancoDeDados);
+        Chamado chamDes = ctrChamados.InserirChamadoDesempenho(titulo, descricao, 0, tecnico, cliente, so, versaoSO, operacao, 0);
+        Chamado chamRede = ctrChamados.InserirChamadoRede(titulo, descricao, 0, tecnico, cliente, so, versaoSO, conexao, enderecoRede);
+        
+        ctrChamados.inserirRegistroChamado("Teste de Banco", chambanco, tec);
+        ctrChamados.inserirRegistroChamado("Teste de Desempenho", chamDes, tec);
+        ctrChamados.inserirRegistroChamado(" Teste de Rede", chamRede, tec);
+        
+        
+        
+        
+        
+        
+        
+    }
     
 }
